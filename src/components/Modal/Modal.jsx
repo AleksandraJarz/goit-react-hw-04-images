9cd82009bfa0b97f724a5e5b64c533a3f04f8ae0
AleksandraJarz <139238278+AleksandraJarz@users.noreply.github.com>
2024-02-21 16:25:47 +0100

    Initial commit

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,12 @@
+import './Modal.css';
+const Modal = ({ handleClick, src }) => {
+  return (
+    <div className="overlay" onClick={handleClick}>
+      <div className="modal">
+        <img src={src} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
